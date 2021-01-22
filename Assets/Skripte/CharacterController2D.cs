6aab2cde864d135046fee9f04085ef54dca549a8
TrapCraftLP <62720869+TrapCraftLP@users.noreply.github.com>
2021-01-22 14:21:48 +0100

    Ich habe versucht nen Character Controller zu schreiben

---
+++ Assets/Skripte/CharacterController2D.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour
+{
+
+    public float moveSpped;
+    public Rigidbody2D rb;
+    Vector3 moveDirection();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs;
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY);
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);   
+    }
+
+}
