d598078ad4aeeaf17e2d229df52b6e3785f68e2c
TrapCraftLP <62720869+TrapCraftLP@users.noreply.github.com>
2021-03-31 16:26:23 +0200

    New things added!
    - Healthbar follows the palyer!
    - press h for heal
    - press space for damage
    - world generator

--- Assets/Skripte/Player.cs
+++ Assets/Skripte/Player.cs
@@ -6,13 +6,14 @@ public class Player : MonoBehaviour
 {
 
     public int maxHealth = 100;
-    public int currntHealth;
+    public int minHealth = 0;
+    public int currentHealth;
     public Healthbar healthBar;
 
     // Start is called before the first frame update
     void Start()
     {
-        currntHealth = maxHealth;
+        currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
     }
 
@@ -21,13 +22,34 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space)) 
         {
-            TakeDamage(20);        
-        }        
+            TakeDamage(15);        
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Heal(15);                   
+        }
+
+
+
     }
     void TakeDamage(int damge)
     {
-        currntHealth -= damge;
-        healthBar.SetHealth(currntHealth);
+        currentHealth -= damge;
+        if (currentHealth < minHealth)
+        {
+            currentHealth = minHealth;
+        }
+        healthBar.SetHealth(currentHealth);
     }
 
+    void Heal(int healValue)
+    {
+        currentHealth += healValue;
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        healthBar.SetHealth(currentHealth);
+    }
 }
---
+++ Assets/Skripte/WorldGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGenerator : MonoBehaviour
+{
+    public GameObject floorTile;
+    public int width;
+    public int height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int w = 0; w < width; w++) 
+        {
+            for (int h = 0; h < height; h++) 
+            {
+                Vector3 floorPosition = new Vector3(w * 4.7f, h * 4.02f, 0);
+                Instantiate(floorTile, floorPosition, Quaternion.identity, transform);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
