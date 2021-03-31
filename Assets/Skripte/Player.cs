603114bc44a1a611e8d280211a61b6c6c4776bee
TrapCraftLP <62720869+TrapCraftLP@users.noreply.github.com>
2021-03-31 09:52:57 +0200

    Added  a plyer script
    healthbar doesn't work...

---
+++ Assets/Skripte/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public int maxHealth = 100;
+    public int currntHealth;
+    public Healthbar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currntHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            TakeDamage(20);        
+        }        
+    }
+    void TakeDamage(int damge)
+    {
+        currntHealth -= damge;
+        healthBar.SetHealth(currntHealth);
+    }
+
+}
