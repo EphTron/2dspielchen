60807e4957a965b2f78fd59ed441158195c24255
TrapCraftLP <62720869+TrapCraftLP@users.noreply.github.com>
2021-03-31 20:45:59 +0200

    Camera Follow
    The Camera is following the Player now!

--- Assets/Skripte/Camerafollow.cs
+++ Assets/Skripte/Camerafollow.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Camerafollow : MonoBehaviour
 {
+    public Transform target; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class Camerafollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
     }
 }
